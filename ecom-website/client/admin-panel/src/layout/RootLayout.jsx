ccb1dfec17d836059a248f36c4b641929a900efe
pradeeprai1990 <pradeep.9997@gmail.com>
2025-02-07 14:56:02 +0530

    latest Code n

--- ecom-website/client/admin-panel/src/layout/RootLayout.jsx
+++ ecom-website/client/admin-panel/src/layout/RootLayout.jsx
@@ -10,7 +10,7 @@ import { Outlet } from 'react-router-dom'
 export default function RootLayout() {
   return (
     <section className="w-full">
-      <div className="grid grid-cols-[16.5%_auto]">
+      <div className="grid grid-cols-[16.5%_auto] gap-2">
         <div>
           <Sidebar />
         </div>
